e9da6acaa6fbabe054e6f2b71fa207551c48c3f3
รัฐธรรม เอิบอิ่มฤทธิ <aum@Macintosh.local>
2023-02-21 16:43:10 +0700

    fix API Path and class name in script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=777aee5fe0f96fb476a0bbe659b5528b&page=1"
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
-const SEARCHAPI = "https://api.themoviedb.org/3/discover/movie?&api_key=777aee5fe0f96fb476a0bbe659b5528b&qyery=\""
-
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=777aee5fe0f96fb476a0bbe659b5528b&query="
 const main = document.getElementById("section")
 const form = document.getElementById("form")
 const search = document.getElementById("query")
@@ -24,7 +23,7 @@ function returnMovies(url){
             div_column.setAttribute('class','column')
 
             const image = document.createElement('img')
-            image.setAttribute('class','thubnail')
+            image.setAttribute('class','thumbnail')
             image.setAttribute("id",'image')
 
             const title = document.createElement('h3')
